09de241eee99e5ebe8f3c9972ef5ac8f28984caa
tiktakdad <karaopea@gmail.com>
2023-04-04 22:11:30 +0900

    add blip inference class

--- BLIP/BLIP_infer.py
+++ BLIP/BLIP_infer.py
@@ -5,25 +5,29 @@ from torchvision import transforms
 from torchvision.transforms.functional import InterpolationMode
 from models.blip import blip_decoder
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
- 
-def inference_BLIP(raw_image):
-    image_size = 384
-    transform = transforms.Compose([
-        transforms.Resize((image_size,image_size),interpolation=InterpolationMode.BICUBIC),
-        transforms.ToTensor(),
-        transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
-    ]) 
-    image = transform(raw_image).unsqueeze(0).to(device)   
-    
-    model_url = './BLIP/pth/model_base.pth'
-    
-    model = blip_decoder(pretrained=model_url, image_size=image_size, vit='base')
-    model.eval()
-    model = model.to(device)
 
-    with torch.no_grad():
-        # beam search
-        caption = model.generate(image, sample=False, num_beams=3, max_length=20, min_length=5)
-    return caption[0]
+class BlipInfer:
+    def __init__(self) -> None:
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.image_size = 384
+        self.load_model()
+
+    def load_model(self):
+        model_url = './BLIP/pth/model_base.pth'
+        self.model = blip_decoder(pretrained=model_url, image_size=self.image_size, vit='base')
+        self.model.eval()
+        self.model = self.model.to(self.device)
+
+    def inference(self, raw_image):
+        transform = transforms.Compose([
+            transforms.Resize((self.image_size, self.image_size),interpolation=InterpolationMode.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+        ]) 
+        image = transform(raw_image).unsqueeze(0).to(self.device)   
+
+        with torch.no_grad():
+            # beam search
+            caption = self.model.generate(image, sample=False, num_beams=3, max_length=20, min_length=5)
+        return caption[0]
 
--- src/image_gen.py
+++ src/image_gen.py
@@ -1,7 +1,7 @@
 
 import torch
 from diffusers import StableDiffusionImg2ImgPipeline, DPMSolverMultistepScheduler
-from BLIP.BLIP_infer import inference_BLIP
+from BLIP.BLIP_infer import BlipInfer
 
 from diffusers import (
     DDIMInverseScheduler,
@@ -34,10 +34,12 @@ class ImageGen:
         # torch_dtype=torch.float16
         # pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id_or_path, torch_dtype=torch.float16)
         self.load_model(device, model_id_or_path)
+        self.blip_infer = BlipInfer()
        
 
     def load_model(self, device, model_id_or_path):
-        self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id_or_path)
+        scheduler = DPMSolverMultistepScheduler.from_pretrained(model_id_or_path, subfolder="scheduler")
+        self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id_or_path, scheduler = scheduler)
         self.pipe.safety_checker = lambda images, clip_input: (images, False)
         #self.pipe.scheduler = DPMSolverMultistepScheduler.from_config(scheduler.config, use_karras_sigmas=True)
         self.pipe = self.pipe.to(device)
@@ -66,27 +68,8 @@ class ImageGen:
     def img2img_clip(self, img):
 
         init_image = img
-        init_image = init_image.resize((512, 768))
-
-        caption = inference_BLIP(init_image)
-
-        device = "cuda"
-        model_id_or_path = "runwayml/stable-diffusion-v1-5"
-        # model_id_or_path = "../model/dump/"
-
-        scheduler = DPMSolverMultistepScheduler.from_pretrained(model_id_or_path, subfolder="scheduler")
-        # pipe.scheduler = DPMSolverMultistepScheduler.from_config(scheduler.config, use_karras_sigmas=True)
-
-        pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
-            model_id_or_path,
-            torch_dtype=torch.float16,
-            scheduler = scheduler
-        )
-        pipe.safety_checker = lambda images, clip_input: (images, False)
-        pipe = pipe.to(device)
-
-        print(pipe.scheduler)
-
+        init_image = init_image.resize((512, 512))
+        caption = self.blip_infer.inference(init_image)
         generator = torch.manual_seed(3306892559)
 
         prompt = "masterpiece, best quality, ultra-detailed," + caption + ", illustration"
@@ -94,7 +77,7 @@ class ImageGen:
         print("eval")
         print("prompt: " + prompt)
         print("negative_prompt: " + negative_prompt)
-        images = pipe(prompt=prompt,negative_prompt=negative_prompt, image=init_image, generator=generator, num_inference_steps=40, guidance_scale=7).images
+        images = self.pipe(prompt=prompt,negative_prompt=negative_prompt, image=init_image, generator=generator, num_inference_steps=40, guidance_scale=7).images
 
         return images[0]
     
