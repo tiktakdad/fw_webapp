4aa035e05b7d60f728ba9ee1557dc90628367f4e
tiktakdad <karaopea@nate.com>
2023-04-04 15:37:30 +0900

    FIX: blip sources

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # fw_webapp
 
+
+## quick start
 ```shell
-$ git clone https://github.com/tiktakdad/fw_webapp.git
-$ (optional) conda create -n fw_webapp python=3.9
-$ pip install -r requirements.txt
-$ python app.py
+git clone https://github.com/tiktakdad/fw_webapp.git
+(optional) conda create -n fw_webapp python=3.9
+(optional) conda activate fw_webapp
+pip install -r requirements.txt
+python app.py
 ```
+## model download
+- https://1drv.ms/u/s!AgGeqTkQEyKRib8iuOxTmLwXERrenA?e=nQyTEh
---
+++ blip/BLIP_infer.py
@@ -0,0 +1,31 @@
+
+
+from PIL import Image
+import requests
+import torch
+from torchvision import transforms
+from torchvision.transforms.functional import InterpolationMode
+from models.blip import blip_decoder
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+ 
+def inference_BLIP(raw_image):
+    image_size = 384
+    transform = transforms.Compose([
+        transforms.Resize((image_size,image_size),interpolation=InterpolationMode.BICUBIC),
+        transforms.ToTensor(),
+        transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
+    ]) 
+    image = transform(raw_image).unsqueeze(0).to(device)   
+    
+    model_url = './BLIP/pth/model_base.pth'
+    
+    model = blip_decoder(pretrained=model_url, image_size=image_size, vit='base')
+    model.eval()
+    model = model.to(device)
+
+    with torch.no_grad():
+        # beam search
+        caption = model.generate(image, sample=False, num_beams=3, max_length=20, min_length=5)
+    return caption[0]
+
